edb388c45a927c047bbf04c9193a924c6c935d47
Mitya Sreda <hauddar@gmail.com>
2023-01-09 23:16:27 +0300

    Expanded tests file

--- chords/functional_tests.py
+++ chords/functional_tests.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'The install worked successfully! Congratulations!' in browser.title
+    def setUp(self) -> None:
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_new_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-DO', self.browser.title)
+        self.fail('End of the test')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
