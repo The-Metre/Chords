294b3247fbf43243acb643cdd2ad971668d8d323
Mitya Sreda <hauddar@gmail.com>
2023-01-10 01:37:54 +0300

    Changed tests

--- pocket_chords/tests.py
+++ pocket_chords/tests.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
+from django.urls import resolve
 
+from pocket_chords.views import home_page
 # Create your tests here.
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
+    ''' test home page'''
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolved_to_home_page_view(self):
+        found = resolve('/')
+
+        self.assertEqual(found.func, home_page)
