1ce551860147ff1e5c936873c08f87416f83132a
Mitya Sreda <hauddar@gmail.com>
2023-01-10 02:51:22 +0300

    added basic tests

--- chords/functional_tests.py
+++ chords/functional_tests.py
@@ -12,7 +12,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_new_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
-        self.assertIn('To-DO', self.browser.title)
+        self.assertIn('To-Do', self.browser.title)
         self.fail('End of the test')
 
 if __name__ == '__main__':
