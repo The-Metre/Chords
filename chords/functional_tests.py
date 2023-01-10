7a570299d656101083da1d7a0b1407ecca562a8a
sreda <mitya.sreda@gmail.com>
2023-01-10 19:25:23 +0300

    functinal tests now check ability to enter an element to the list

--- chords/functional_tests.py
+++ chords/functional_tests.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,6 +17,28 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
+
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element(By.TAG_NAME, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        
+        inputbox.send_keys('Group name')
+        inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Group name' for row in rows)
+        )
+
+
         self.fail('End of the test')
 
 if __name__ == '__main__':
