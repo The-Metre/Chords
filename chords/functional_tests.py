553d0258ebc997394f95e697d758400b34800bbe
sreda <mitya.sreda@gmail.com>
2023-01-10 21:24:39 +0300

    changed homepage.html file

--- chords/functional_tests.py
+++ chords/functional_tests.py
@@ -19,12 +19,12 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
 
         header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
-        self.assertIn('To-Do', header_text)
+        self.assertIn('Music', header_text)
 
-        inputbox = self.browser.find_element(By.TAG_NAME, 'id_new_item')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
+            'Enter a song name'
         )
         
         inputbox.send_keys('Group name')
@@ -32,10 +32,11 @@ class NewVisitorTest(unittest.TestCase):
 
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID, 'id_list_table')
+        table = self.browser.find_element(By.ID, 'id_song_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
         self.assertTrue(
-            any(row.text == '1: Group name' for row in rows)
+            any(row.text == '1: Group name' for row in rows),
+            "New element doesn't show in the table"
         )
 
 
