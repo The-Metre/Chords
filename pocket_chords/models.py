65defc4624b84a8488538e558fb89df5d5ccb494
sreda <mitya.sreda@gmail.com>
2023-01-11 00:37:46 +0300

    added new models class and bounded migrations with it

--- pocket_chords/models.py
+++ pocket_chords/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Song(models.Model):
+    name = models.CharField(default='unknows', max_length=255)
+    text = models.TextField(default='')
--- pocket_chords/tests.py
+++ pocket_chords/tests.py
@@ -1,5 +1,26 @@
 from django.test import TestCase
+from pocket_chords.models import Song
 # Create your tests here.
+
+class SongModelTest(TestCase):
+    ''' test Song model '''
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Song()
+        first_item.text = 'The first(ever) list item'
+        first_item.save()
+
+        second_item = Song()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Song.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first(ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
 
 class HomePageTest(TestCase):
     ''' test home page'''
