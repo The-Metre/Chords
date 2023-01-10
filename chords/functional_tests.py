a6ed00f7220ad69bbdb6319152dabcb1f3260c19
sreda <mitya.sreda@gmail.com>
2023-01-11 00:01:47 +0300

    changed func tests

--- chords/functional_tests.py
+++ chords/functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_song_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_new_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
@@ -33,16 +38,34 @@ class NewVisitorTest(unittest.TestCase):
         )
         
         # Try to fill the info in form space and press 'enter' key
-        inputbox.send_keys('Group name')
+        inputbox.send_keys('Group name one')
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Group name one')
+        time.sleep(1)
 
+        
+        # Try to fill the second info in form space and press 'enter' key
+        inputbox = self.browser.find_element(By.ID, 'id_new_song_name')
+        inputbox.send_keys('Group name second')
+        inputbox.send_keys(Keys.ENTER)
+        
         time.sleep(1)
 
         # Check the table tag and correct rows
         table = self.browser.find_element(By.ID, 'id_song_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
+        
+        
+        self.check_for_row_in_list_table('1: Group name one')
+        self.check_for_row_in_list_table('2: Group name second')
+        
         self.assertIn(
-            '1: Group name', [row.text for row in rows],
+            '1: Group name one', [row.text for row in rows],
+            f"New element doesn't show in the table, table contain: \n{table.text}"
+        )
+
+        self.assertIn(
+            '2: Group name one', [row.text for row in rows],
             f"New element doesn't show in the table, table contain: \n{table.text}"
         )
 
