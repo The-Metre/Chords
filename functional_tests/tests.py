1bd4c8e7fdb55780cd703df804dc21a68864c0cf
sreda <mitya.sreda@gmail.com>
2023-01-11 22:35:27 +0300

    changed functional tests unittests to LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,12 @@
+from django.test import LiveServerTestCase
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self) -> None:
         self.browser = webdriver.Chrome()
@@ -19,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_new_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # Check the correct title
         self.assertIn('To-Do', self.browser.title)
@@ -70,7 +71,7 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         # End of the test
-        #self.fail('End of the test')
+        self.fail('End of the test')
 
 if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+    LiveServerTestCase.main(warnings='ignore')
