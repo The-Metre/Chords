e5d7102db5844cd48edb338b580c7a19d088810c
sreda <mitya.sreda@gmail.com>
2023-01-20 19:25:04 +0300

    fixed functional tests

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -7,7 +7,6 @@ class ItemValidationTest(FunctionalTest):
 
     def test_cannot_empty_list(self):
         """ Test cannot add empty element in the list """
-
         # End of the test
         self.fail('End of the test')
 
