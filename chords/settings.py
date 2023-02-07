a61f64a08d7071489e48072592f502ebbe1af031
sreda <mitya.sreda@gmail.com>
2023-02-07 15:31:10 +0300

    fixed functional tests

--- chords/settings.py
+++ chords/settings.py
@@ -146,7 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_USER = 'authenticator.sreda.test.app@gmail.com'
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
