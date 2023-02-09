a68efb9ab98df091ea0f9c60c0edea2f79f9d1db
sreda <mitya.sreda@gmail.com>
2023-02-09 20:28:27 +0300

    added new decorator (wait) for functional tests

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -21,55 +21,53 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def tearDown(self) -> None:
         self.browser.quit()
-
+    
+    def wait(fn):
+        def modified_fn(*args, **kwargs):
+            start_time = time.time()
+            while True:
+                try:
+                    return fn(*args, **kwargs)
+                except (AssertionError, WebDriverException) as e:
+                    if time.time() - start_time > MAX_WAIT:
+                        raise e
+                    time.sleep(0.5)
+        return modified_fn
+    
+    @wait
     def wait_for_row_in_list_table(self, row_text):
         """ wait for row in the table """
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element(By.ID, 'id_song_table')
-                rows = table.find_elements(By.TAG_NAME, 'tr')
-                
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except(AssertionError, WebDriverException) as err:
-                if time.time() - start_time > MAX_WAIT:
-                    raise err
-                time.sleep(0.5)
+        table = self.browser.find_element(By.ID, 'id_song_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        
+        self.assertIn(row_text, [row.text for row in rows])
 
+    @wait
     def wait_for(self, func):
         """ Wait for """
-        start_time = time.time()
-        while True:
-            try:
-                return func()
-            except(AssertionError, WebDriverException) as err:
-                if time.time() - start_time > MAX_WAIT:
-                    raise err
-                time.sleep(0.5)
+        return func()
 
+    @wait
     def wait_to_be_logged_in(self, email):
         """ Test: login to the system
             and trying to find our email dress 
             on a page
         """
-        print(self.browser)
-        self.wait_for(
-            lambda: self.browser.find_element(By.LINK_TEXT, 'Log out')
-        )
+        self.browser.find_element(By.LINK_TEXT, 'Log out')
         navbar = self.browser.find_element(By.CSS_SELECTOR, '.navbar')
         self.assertIn(email, navbar.text)
 
+    @wait
     def wait_to_be_logged_out(self, email):
         """ Test: log out from the system
             page should not contains our email 
         """
-        self.wait_for(
-            lambda: self.browser.find_element(By.NAME, 'email')
-        )
+        self.browser.find_element(By.NAME, 'email')
         navbar = self.browser.find_element(By.CSS_SELECTOR, '.navbar')
         self.assertNotIn(email, navbar.text)
 
+
+    
 
     def get_song_input_box(self):
         return self.browser.find_element(By.ID, 'id_name')
