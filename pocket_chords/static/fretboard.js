dfc65364d3fd54ab84db85793559cfddda93bd88
sreda <mitya.sreda@gmail.com>
2023-02-25 10:10:07 +0300

    refactoring freboard.js file

--- pocket_chords/static/fretboard.js
+++ pocket_chords/static/fretboard.js
@@ -1,7 +1,7 @@
 const root = document.documentElement;
 
 const fretboard = document.querySelector('.fretboard');
-const selected_instrument_selector = document.querySelector('#instrument-selector'); 
+const instrument_selector = document.querySelector('#instrument-selector'); 
 const accidentals_selector = document.querySelector('.accidental-selector'); 
 const number_of_frets_selector = document.querySelector('#number-of-frets');
 const show_all_notes_selector = document.querySelector('#show-all-notes');
@@ -22,7 +22,7 @@ const instrument_tuning_presets = {
 
 let all_notes;
 let show_duplicate_notes = false;
-
+let show_all_notes = false;
 let selected_instrument = 'Guitar';
 let number_of_frets = 12;
 let number_of_strings = instrument_tuning_presets[selected_instrument].length;
@@ -86,7 +86,7 @@ const fretboard_app = {
     setup_selected_instrument() {
         for (instrument in instrument_tuning_presets) {
             let instrument_option = fretboard_tools.createElement('option', instrument);
-            selected_instrument_selector.appendChild(instrument_option);
+            instrument_selector.appendChild(instrument_option);
         }
     },
     /*  */
@@ -103,8 +103,15 @@ const fretboard_app = {
             note_name_section.appendChild(note_name_element);
         });
     },
-    /*  */
+    
     show_note_dot(event) {
+    /*  Add listeners to each fret, when hover on it by the mouse
+        change opacity, so a note of the fret will be visible
+    */  
+        // Check if show all notes is selected
+        if (show_all_notes) {
+            return;
+        }
         if (event.target.classList.contains('fret-note')) {
             if (show_duplicate_notes) {
                 fretboard_app.toggle_duplicate_notes(event.target.getAttribute('note-data'), 1);
@@ -113,8 +120,15 @@ const fretboard_app = {
             }
         }
     },
-    /*  */
+
     hide_note_dot(event) {
+    /*  When mouse out from the fret, change it opactity
+        to zero
+    */
+        // Check if show all notes is selected
+        if (show_all_notes) {
+            return;
+        }
         if (show_duplicate_notes) {
             fretboard_app.toggle_duplicate_notes(event.target.getAttribute('note-data'), 0);
         } else {
@@ -122,81 +136,82 @@ const fretboard_app = {
         }
     },
 
-    setup_event_listeners() {
-        // Add listeners to each fret, when hover on it by the mouse
-        // change opacity, so a note of the fret will be visible
-        fretboard.addEventListener('mouseover', this.show_note_dot);
-        // When mouse out from the fret, change it opactity
-        // to zero
-        fretboard.addEventListener('mouseout', this.hide_note_dot);
-
-        // Whe user switch instrument options,
-        // change number of strings value, based on an instrument
-        selected_instrument_selector.addEventListener('change', (event) => {
-            selected_instrument = event.target.value;
-            number_of_strings = instrument_tuning_presets[selected_instrument].length;
-            this.setup_fretboard();
-        });
-        // Whe user switch accidental options,
-        // change a type of notes notation (flats or sharps)
-        accidentals_selector.addEventListener('click', (event) => {
-            if (event.target.classList.contains('acc-select')) {
-                accidentals = event.target.value;
-                this.setup_fretboard();
-                this.setup_note_name_section();
-            } else {
-                return;
-            }
-        });
-        // Add listener to input number,
-        // that change number of rendered frets of an instrument
-        number_of_frets_selector.addEventListener('change', (event) => {
-            number_of_frets = number_of_frets_selector.value;
-            this.setup_fretboard();
-        });
-        //
-        show_all_notes_selector.addEventListener('change', () => {
-            // Remove event listeners from notes, when
-            // show-all-notes flag are checked
-            if (show_all_notes_selector.checked) {
-                root.style.setProperty('--note-dot-opacity', 1);
-
-                fretboard.removeEventListener('mouseover', this.show_note_dot);
-                fretboard.removeEventListener('mouseout', this.hide_note_dot);
-                this.setup_fretboard();
-                // Add the event listeners if flag removed
-            } else {
-                root.style.setProperty('--note-dot-opacity', 0);
-
-                fretboard.addEventListener('mouseover', this.show_note_dot);
-                fretboard.addEventListener('mouseout', this.hide_note_dot);
-                this.setup_fretboard();
-            }
-        });
-        /* Realization of a toggle event on the checkbox */
-        show_duplicate_notes_selector.addEventListener('change', () => {
-            show_duplicate_notes = !show_duplicate_notes;
-        });
-        /*  */
-        note_name_section.addEventListener('mouseover', (event) => {
-             let note_to_show = event.target.innerText;
-             fretboard_app.toggle_duplicate_notes(note_to_show, 1);
-        });
-        /*  */
-        note_name_section.addEventListener('mouseout', (event) => {
-            if (!show_all_notes_selector.checked) {
-                let note_to_show = event.target.innerText;
-                fretboard_app.toggle_duplicate_notes(note_to_show, 0);
-            } else {
-                return;
-            }
-        });
+    set_selected_instrument(event) {
+    /*  Whe user switch instrument options,
+        change number of strings value, based on an instrument  
+    */
+        selected_instrument = event.target.value;
+        number_of_strings = instrument_tuning_presets[selected_instrument].length;
+        fretboard_app.setup_fretboard();
     },
 
+    set_accidentals(event) {
+    /*  Whe user switch accidental options, 
+        change a type of notes notation (flats or sharps)
+    */
+        if (event.target.classList.contains('acc-select')) {
+            accidentals = event.target.value;
+            fretboard_app.setup_fretboard();
+            fretboard_app.setup_note_name_section();
+        } else {
+            return;
+        }
+    },
+
+    set_number_of_frets(event) {
+    // Add listener to input number,
+    // that change number of rendered frets of an instrument
+        number_of_frets = number_of_frets_selector.value;
+        fretboard_app.setup_fretboard();
+    },
+    
+    set_show_all_notes() {
+        // Set notes opacity to 1(visible) if 
+        // show all notes selected
+        show_all_notes = show_all_notes_selector.checked;
+        if (show_all_notes_selector.checked) {
+            root.style.setProperty('--note-dot-opacity', 1);
+            fretboard_app.setup_fretboard();
+        // If show all notes not selected
+        } else {
+            root.style.setProperty('--note-dot-opacity', 0);
+            fretboard_app.setup_fretboard();
+        }
+    },
+    set_show_duplicate_notes() {
+    /* Realization of a toggle event on the checkbox */
+        show_duplicate_notes = !show_duplicate_notes;
+    },
+    set_notes_to_show(event) {
+        let note_to_show = event.target.innerText;
+        fretboard_app.toggle_duplicate_notes(note_to_show, 1);
+    },
+    set_notes_to_hide(event) {
+        if (!show_all_notes_selector.checked) {
+            let note_to_show = event.target.innerText;
+            fretboard_app.toggle_duplicate_notes(note_to_show, 0);
+        } else {
+            return;
+        }
+    },
+    setup_event_listeners() {
+
+        fretboard.addEventListener('mouseover', this.show_note_dot);
+        fretboard.addEventListener('mouseout', this.hide_note_dot);
+        instrument_selector.addEventListener('change', this.set_selected_instrument);
+        accidentals_selector.addEventListener('click', this.set_accidentals);
+        number_of_frets_selector.addEventListener('change', this.set_number_of_frets);
+        show_all_notes_selector.addEventListener('change', this.set_show_all_notes);
+        show_duplicate_notes_selector.addEventListener('change', this.set_show_duplicate_notes);
+        note_name_section.addEventListener('mouseover', this.set_notes_to_show);
+        note_name_section.addEventListener('mouseout', this.set_notes_to_hide);
+    },
+
+
+    toggle_duplicate_notes(note_name, opacity) {
     /* When checkbox flag toggled
         change opacity of a target note
     */
-    toggle_duplicate_notes(note_name, opacity) {
         all_notes = document.querySelectorAll('.fret-note');
         for (let i = 0; i < all_notes.length; i++) {
             if (all_notes[i].getAttribute('note-data') === note_name) {
