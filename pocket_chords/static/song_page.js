e747fdb7ed150d2937186198332ccdf345455202
sreda <mitya.sreda@gmail.com>
2023-03-30 21:04:43 +0300

    added regex js to song_page.js file

--- pocket_chords/static/song_page.js
+++ pocket_chords/static/song_page.js
@@ -2,7 +2,11 @@ let edit = id => {
     let element = document.getElementById(id).getElementsByClassName('edit-chunk')[0];
     element.hidden = true;
     let save = document.getElementById(id).getElementsByClassName('save-chunk')[0];
-    save.hidden = false
+    save.hidden = false;
+    let text = document.getElementById(id).getElementsByClassName('chunk-input')[0];
+    text.hidden = false;
+    let some = document.getElementById(id)
+    some.display = 'none'
 }
 
 let save = id => {
@@ -12,3 +16,13 @@ let save = id => {
     save.hidden = true;
     console.log('saving', id)
 }
+
+
+const text = document.querySelector('.song_text')
+const item = 'Em'
+const chunk = new RegExp(item, "g")
+
+function rep() {
+    text.innerHTML
+        = text.innerHTML
+        .replace(chunk, `<span class='chord' value='${item}'>${item}</span>`)};
