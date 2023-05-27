897ac3e89b5c8683f06fe280875d586857fb31e7
sreda <mitya.sreda@gmail.com>
2023-05-27 21:17:58 +0300

    changed UserManager in models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,29 @@
 from django.db import models
 import uuid
-from django.contrib.auth.models import PermissionsMixin, AbstractBaseUser, UserManager
+from django.contrib.auth.models import PermissionsMixin, AbstractBaseUser, BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(primary_key=True, unique=True)
@@ -14,9 +37,9 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-
     def __str__(self):
         return self.email
+        
 
 class Token(models.Model):
     email = models.EmailField()
