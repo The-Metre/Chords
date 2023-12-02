c3c7f1f024a33d4d9fe4dd6b5588cb9658d6670f
Mitya Sreda <mitya.sreda@gmail.com>
2023-12-02 22:17:46 +0300

    added new practice page tests

--- functional_tests/test_practice_page.py
+++ functional_tests/test_practice_page.py
@@ -4,13 +4,21 @@ from selenium.webdriver.common.by import By
 import re
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 
 from .base import FunctionalTest
 
 
 
+"""  """
+from pocket_chords.models import MusicNote, Chord, ChordNotesRelation
+from project_tools.classes import GuitarStuffClass
+"""  """
+
 TEST_EMAIL = 'firstone@example.com'
+TEST_FIRST_CHORD_NAME = 'a minor'
+TEST_SECOND_CHORD_NAME = 'f major'
+TEST_FIRST_CHORD_NOTES = 'A C E'
+TEST_SECOND_CHORD_NOTES = 'F A C'
 EMAIL_USERNAME, _ = TEST_EMAIL.split('@')
 
 class LoginTest(FunctionalTest):
@@ -40,6 +48,21 @@ class LoginTest(FunctionalTest):
         self.browser.quit()
 
 
+    def _fill_the_chord_with_notes(self, key_note):
+        """ 
+        function to fill music class object with notes
+        """
+        root = MusicNote.objects.get(name=key_note)
+        scale_from_root = self.guitar.string_tuning(root.name)
+
+        for chord_name in self.guitar._chord_formula:
+            # Fill Chord sql table with chord name and related root note
+            chord, _ = Chord.objects.get_or_create(name=f"{root.name} {chord_name}", root_note=root)
+            notes = self.guitar._chord_formula[chord_name]
+            for note in notes:
+                # Fill Chord-Notes relationship table
+                chord_note = MusicNote.objects.get(name=scale_from_root[note])
+                ChordNotesRelation.objects.get_or_create(chord_name=chord, chord_note=chord_note)
 
     def test_render_practice_page(self):
         """ check that browser render practice page
@@ -62,3 +85,58 @@ class LoginTest(FunctionalTest):
         self.wait_for(link_button.click)
         # check that redirect on the current user practice page
         self.assertRegex(self.browser.current_url, f'/practice/{EMAIL_USERNAME}.+')
+
+    def test_not_render_current_user_practice_page(self):
+        """
+        check that link don't redirect to the current user practice page 
+        """
+        link_button = self.browser.find_element(By.LINK_TEXT, "My songs")
+        self.wait_for(link_button.click)
+        # check that not redirect on the current user practice page
+        self.assertNotRegexpMatches(self.browser.current_url, f'/pracice/{EMAIL_USERNAME}.+')
+
+    def test_fretboard_appearance(self):
+        """ 
+        test visibility of a fretboard when click on checkbox
+        """
+        # open a practice page
+        self.wait_for(self.browser.find_element(By.LINK_TEXT, 'Practice').click)
+        checkbox = self.browser.find_element(By.ID, 'show-guitar-fretboard')
+        fretboard = self.browser.find_element(By.CLASS_NAME, 'fretboard')
+        # checkbox attribute 'checked' by default should be true
+        self.assertTrue(checkbox.get_attribute('checked'))
+        # fretboard is visible
+        self.assertTrue(fretboard.is_displayed())
+        # click on the checkbox
+        self.wait_for(checkbox.click)
+        # fretboard should be hide
+        self.assertFalse(checkbox.get_attribute('checked'))
+        self.assertFalse(fretboard.is_displayed())
+    
+    def test_autocomplete_input(self):
+        """ 
+        test autocomplete input functionality of the practice page
+        """
+        # setup the postgres for the test
+        self.guitar = GuitarStuffClass()
+        # Fill MusicNote table with all note names
+        for note in self.guitar._notes:
+            MusicNote.objects.get_or_create(name=note)
+
+        # For each created note, create a related chord
+        for note in MusicNote.objects.all():
+            self._fill_the_chord_with_notes(note.name)
+
+
+        # open a practice page
+        self.wait_for(self.browser.find_element(By.LINK_TEXT, 'Practice').click)
+        div_with_chord_notes = self.browser.find_element(By.CLASS_NAME, 'chord-notes')
+        # type a chord name in the input field
+        input_box = self.browser.find_element(By.ID, 'autocomplete-input')
+        input_box.send_keys(TEST_FIRST_CHORD_NAME)
+        input_box.send_keys(Keys.ENTER)
+        # check correct output
+        self.assertEqual(div_with_chord_notes.text, TEST_FIRST_CHORD_NOTES)
+
+
+
